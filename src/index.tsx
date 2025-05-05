58464ed6bf2b3400d7e2b38c8a03bc2bc49e7c70
eloanmf <eloanmf@hotmail.com>
2025-05-05 16:16:57 -0300

    fix: removendo

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client';
 //
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-// ----------------------------------------------------------------------
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
